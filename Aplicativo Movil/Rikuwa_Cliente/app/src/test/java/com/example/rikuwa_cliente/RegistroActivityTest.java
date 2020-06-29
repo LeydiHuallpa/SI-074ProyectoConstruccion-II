7393aa124dab45c424803a75c216550b0f846632
oreramirez <oreramirezt@gmail.com>
2020-06-29 01:40:52 -0500

    test
    tests

---
+++ Aplicativo Movil/Rikuwa_Cliente/app/src/test/java/com/example/rikuwa_cliente/RegistroActivityTest.java
@@ -0,0 +1,36 @@
+package com.example.rikuwa_cliente;
+
+import junit.framework.TestCase;
+
+public class RegistroActivityTest extends TestCase {
+
+    public void testOnCreate() {
+    }
+
+    public void testMtdDesactivarInputs() {
+    }
+
+    public void testMtdActivarInputs() {
+    }
+
+    public void testMtdMostrarProgress() {
+    }
+
+    public void testMtdOcultarProgress() {
+    }
+
+    public void testMtdHandleRegistro() {
+    }
+
+    public void testMtdValidarViews() {
+    }
+
+    public void testOnDestroy() {
+    }
+
+    public void testMtdOnError() {
+    }
+
+    public void testMtdOnLogin() {
+    }
+}
